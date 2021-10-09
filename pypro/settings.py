eb0074c1d7539cc6ab7ff0d0e80da13ee3daf77a
jasiel <jasiel_serra@hotmail.com>
2021-10-09 15:39:38 -0300

    Configurado Python-decouple, variaveis de ambiente modo debug or produção.

--- pypro/settings.py
+++ pypro/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p7i0igd(sbkhi+=dsw9=)b-x6dv&6300&rkck8paxkemwaq_#*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
