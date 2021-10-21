d653ebbbc27711d57eebf1916754a87818c83e19
jasiel <jasiel_serra@hotmail.com>
2021-10-21 20:40:37 -0300

    Implementado asserts de conteudo da app aperitivos.

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from pypro.django_assertions import assert_contains
 
 # Create your tests here.
 @pytest.fixture
@@ -8,3 +9,9 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/251224475"')
