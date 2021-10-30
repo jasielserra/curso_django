a1f5cf7c00135eed3b8644eb7e03799cd0278c4c
jasiel Serra <jasiel_serra@hotmail.com>
2021-10-29 22:35:02 -0300

    Utilizando filtros nos templates do Django.

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def video(request, slug):
-   video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 251224475}
+   video = {
+      'instalacao-windows':{'titulo': '', 'vimeo_id': 251497668},
+      'motivacao':{'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 251224475},
+   }
+   video=video[slug]
    return render(request, 'aperitivos/video.html', context={'video': video})
