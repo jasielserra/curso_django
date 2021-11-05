a2fcf2c392fb18dd693e3750ca33d2e77f4f52f7
jasiel Serra <jasiel_serra@hotmail.com>
2021-11-04 22:00:51 -0300

    Construção da Página de Detalhes do Módulo

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -5,3 +5,7 @@ from pypro.modulos.models import Modulo
 
 def listar_modulos_ordenados() -> List[Modulo]:
     return list(Modulo.objects.order_by('order').all())
+
+
+def encontrar_modulo(slug: str) -> Modulo:
+    return Modulo.objects.get(slug=slug)
