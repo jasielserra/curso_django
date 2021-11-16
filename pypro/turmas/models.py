96299642213c41042835c379c043a63368cdfc5d
jasiel Serra <jasiel_serra@hotmail.com>
2021-11-16 17:03:11 -0300

    Revertida Migrações

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -7,4 +7,3 @@ class Turma(models.Model):
     slug = models.SlugField(max_length=64)
     inicio = models.DateField()
     fim = models.DateField()
-    matriculas = models.ManyToManyField(get_user_model())
