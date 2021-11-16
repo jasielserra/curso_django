bd2614f131eac27abb18e1439314245d24e22bd0
jasiel Serra <jasiel_serra@hotmail.com>
2021-11-16 16:07:06 -0300

    Criando Campo ManyToManyField

--- pypro/turmas/admin.py
+++ pypro/turmas/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from pypro.turmas.models import Turma
 # Register your models here.
+
+
 @admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
     list_display = ('nome','slug','inicio','fim')
