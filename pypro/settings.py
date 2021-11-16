fcf6af6771c929b6e0660f0faa89511ac495dee3
jasiel Serra <jasiel_serra@hotmail.com>
2021-11-15 22:31:52 -0300

    Criada App Turmas

--- pypro/settings.py
+++ pypro/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pypro.base',
     'pypro.aperitivos',
+    'pypro.turmas',
     'pypro.modulos',
     'ordered_model',
     'django_extensions',
