b4c823b10da7c7558656efe6672e58f076e21c37
jasiel Serra <jasiel_serra@hotmail.com>
2021-11-21 11:03:45 -0300

    Dados de Usuário no Cabeçalho

--- pypro/conftest.py
+++ pypro/conftest.py
@@ -3,7 +3,7 @@ from model_mommy import mommy
 
 @pytest.fixture
 def usuario_logado(db, django_user_model):
-    usuario_modelo = mommy.make(django_user_model)
+    usuario_modelo = mommy.make(django_user_model, first_name='Fulano')
     return usuario_modelo
 
 @pytest.fixture
